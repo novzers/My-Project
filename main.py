853cb0bd576164165b1cd061be168dfe35d9716e
Malik Akbar Hashemi Rafsanjani <pro.malikakbar2357@gmail.com>
2021-10-02 18:15:42 +0700

    add factorial function

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # Implement factorial function
-
+def factorial(n):
+  sum = 1
+  for i in range(1,n+1):
+    sum *= i
+  return sum
 
 def main():
   print("main here")
+  print(factorial(4))
   
 if __name__ == '__main__':
   main()
